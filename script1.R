c5274734cddfbd14670fe71900279a06ef26454a
Lars-Gustav Snipen <larssn@NMBU.NO>
2018-04-20 10:39:46 +0200

    -reading a single file - converting publish_time to date-time

--- script1.R
+++ script1.R
@@ -1,1 +1,13 @@
 # This is the first script
+library(readr)
+library(lubridate)
+library(dplyr)
+
+### Reading data from file
+tab <- read_csv("data/reuters-newswire-2011.csv.zip", 
+                  col_types=cols(publish_time=col_character()))
+
+### Converting column publish_time to date-time
+tab %>%
+  mutate(publish_time=ymd_hm(publish_time)) -> tab
+
