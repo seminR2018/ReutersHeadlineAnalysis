852afdf7de2c1b7fb8d88f505215749c2445d7a3
Raju Rimal <therimalaya@gmail.com>
2018-04-20 14:07:06 +0200

    Basic count data generated

--- script.R
+++ script.R
@@ -2,12 +2,12 @@ library(tidyverse)
 library(tidytext)
 
 ## ---- Read CSV file and save the final table in feather format ----
-if (file.exists("_data/newswire.feather")) {
-  dta <- feather::read_feather("_data/newswire.feather")
+if (file.exists("data/newswire.feather")) {
+  dta <- feather::read_feather("data/newswire.feather")
 } else {
-  files <- dir("_data", pattern = "csv")
+  files <- dir("data", pattern = "csv")
   dta <- map_df(files, function(file) {
-    read_csv(paste0("_data/", file))
+    read_csv(paste0("data/", file))
   })
   
   dta <- dta %>%
@@ -18,6 +18,29 @@ if (file.exists("_data/newswire.feather")) {
 }
 
 ## ---- Read the feather format of the data ----
-dta %>% 
+word_count <- function(txt, token, ...){
+  tbl <- tibble(text = txt)
+  out <- tbl %>% unnest_tokens(col_name, text, token, ...) %>% 
+    nrow()
+  return(out)
+}
+
+with_counts <- dta %>% 
+  sample_n(1000) %>% 
   rowwise() %>% 
-  
+  mutate(word = word_count(headline_text, "words"),
+         character = word_count(headline_text, "characters"),
+         word_den = word/character,
+         punct = map_int(headline_text, ~nchar(gsub("[^[:punct:]]", "", .x))),
+         upper = word_count(headline_text, "regex", pattern = "[A-Z]"),
+         lower = word_count(headline_text, "regex", pattern = "[a-z]"),
+         ul_ratio = upper/lower)
+
+with_counts %>% 
+  select(Year, word, character, word_den, punct, upper, lower) %>% 
+  gather(type, value, -Year) %>% 
+  ggplot(aes(Year, value)) +
+  facet_wrap(~type, scales = 'free_y') +
+  stat_summary(fun.data = mean_se, geom = "errorbar", color = "red", width = 0.3) +
+  stat_summary(fun.y = mean, geom = "point") +
+  stat_summary(fun.y = mean, geom = "line", group = 1)
